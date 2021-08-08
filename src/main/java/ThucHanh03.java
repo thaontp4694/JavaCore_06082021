a3614f231a71fbe85f683afa6da9a8d9117cfbe4
thaontp106 <thaonguyen040694@gmail.com>
2021-08-08 19:42:46 +0700

    - Bài thực hành 3 - Not complete bài 2

--- src/main/java/ThucHanh03.java
+++ src/main/java/ThucHanh03.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class ThucHanh03 {
+import java.util.Scanner;
+
+public class ThucHanh03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        Bài 1:
+//        Viết chương trình xét 1 năm có phải năm nhuận hay không?
+//        Năm nhuận là năm chia hết cho 400 hoặc chia hết cho 4 đồng thời không chia hết cho 100.
+        System.out.println("Bài 1");
+        System.out.print("Input year: ");
+        int year1 = scanner.nextInt();
+        if ((year1 % 4 == 0 && year1 % 100 != 0) || (year1 % 400 == 0)) {
+            System.out.println("This is a leap year");
+        } else {
+            System.out.println("This is not a leap year");
+        }
+    }
 }
